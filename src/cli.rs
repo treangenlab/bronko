9d88af70f89a9e12012f14ccd2c81f81c868ef1b
Ryan Doughty <rdd4@p00.cs.rice.edu>
2025-08-16 14:59:37 -0500

    add comment to cli depth arg

--- src/cli.rs
+++ src/cli.rs
@@ -98,6 +98,7 @@ pub struct QueryArgs {
     #[clap(long="n-per-strand", default_value_t = DEFAULT_N_KMERS_PER_STRAND, help_heading="VARIANT CALLING PARAMETERS", help="Minimum number of unique kmers to observe on each strand to call a variant at any site")]
     pub n_per_strand: usize,
 
+    //min depth to call a variant
     #[clap(long="min-depth", default_value_t = DEFAULT_MIN_DEPTH, help_heading="VARIANT CALLING PARAMETERS", help="Minimum depth at an allele to call a variant (default=100*min_kmers)")]
     pub min_depth: usize,
 
