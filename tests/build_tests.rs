71478cebadeeebfb026c66bcfddc2f4a78fe7209
Ryan Doughty <57912116+rdoughty10@users.noreply.github.com>
2025-10-16 09:40:11 -0500

    Update build_tests.rs to single sequential test

--- tests/build_tests.rs
+++ tests/build_tests.rs
@@ -1,7 +1,7 @@
 use assert_cmd::Command;
 
 #[test]
-fn test_build_4_genomes() {
+fn build_tests() {
     let output_dir = tempfile::tempdir().unwrap();
     let output_path = output_dir.path().join("bronko");
 
@@ -19,44 +19,32 @@ fn test_build_4_genomes() {
         .assert()
         .success()
         .code(0);
-}
 
-#[test]
-fn test_build_single_genome_and_k19() {
-    let output_dir = tempfile::tempdir().unwrap();
-    let output_path = output_dir.path().join("hpv_index");
-
-    Command::cargo_bin("bronko")
-        .unwrap()
-        .args([
-            "build",
-            "-g",
-            "test_data/HPV16.fa",
-            "-k",
-            "19",
-            "-t",
-            "2",
-            "-o",
-            output_path.to_str().unwrap(),
-        ])
+    let mut cmd = Command::cargo_bin("bronko").unwrap();
+    cmd.arg("build")
+        .arg("-g")
+        .arg("test_data/HPV16.fa")
+        .arg("-k")
+        .arg("19")
+        .arg("-t")
+        .arg("2")
+        .arg("-o")
+        .arg(&output_path)
         .assert()
         .success()
         .code(0);
-}
 
-#[test]
-fn test_build_single_genome() {
-
-    Command::cargo_bin("bronko")
-        .unwrap()
-        .args([
-            "build",
-            "-g",
-            "-t",
-            "2",
-            "test_data/HPV16.fa",
-        ])
+    let mut cmd = Command::cargo_bin("bronko").unwrap();
+    cmd.arg("build")
+        .arg("-g")
+        .arg("test_data/HPV16.fa")
+        .arg("-t")
+        .arg("2")
+        .arg("-o")
+        .arg(&output_path)
         .assert()
         .success()
         .code(0);
+
 }
+
